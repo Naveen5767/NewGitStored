ad20af5fda2d515ce8d522254d4e077553f43021
Naveen5767 <mknaveen5767@gmail.com>
2022-06-13 18:09:07 +0530

    done mycoding

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+public class Sample {
+	
+	public void empId() {
+		System.out.println("EmpId : 25346");
+	}
+	
+	public void empName() {
+		System.out.println("EmpName : Naveen");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
